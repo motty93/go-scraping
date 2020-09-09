134efcd0a2ce23b562a14578e2a882fe9e4bd490
motty93 <rdwbocungelt5@gmail.com>
2020-09-09 22:09:04 +0900

    update: morris guitar site scraping

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape() {
-	// Request the HTML page.
-	res, err := http.Get("http://metalsucks.net")
+func scrape() {
+	res, err := http.Get("https://moridaira.jp/morris/performers-edition-morris")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,21 +19,21 @@ func ExampleScrape() {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
-	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Find the review items
-	doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
-		band := s.Find("a").Text()
-		title := s.Find("i").Text()
-		fmt.Printf("Review %d: %s - %s\n", i, band, title)
-	})
+	selection := doc.Find("div.morrisshowbox")
+	selection.Each(findElements)
+}
+
+func findElements(i int, s *goquery.Selection) {
+	title := s.Text()
+	// title := s.Find("a").Find("img").Text()
+	fmt.Printf("title: %s\n", title)
 }
 
 func main() {
-	ExampleScrape()
+	scrape()
 }
